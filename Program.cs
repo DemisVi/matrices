39f74a3084f527a31c49386926f7412b2f4ccf91
DemisVi <87558735+DemisVi@users.noreply.github.com>
2022-10-07 03:12:26 +0300

    Segment-Thread inconsistance

--- Program.cs
+++ Program.cs
@@ -1,56 +1,54 @@
 ﻿using System;
 using Matrix;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 
 namespace Matrix;
 
-public static class SimpleBench
+public class SimpleBench
+{
+    private float[,] m1 = new float[10, 10];
+    private float[,] m2 = new float[10, 10];
+    private float[,] m3 = new float[10, 10];
+    private float[,] m4 = new float[10, 10];
+    public SimpleBench()
+    {
+        FillRandom(ref m1);
+        FillRandom(ref m2);
+        FillRandom(ref m3);
+        FillRandom(ref m4);
+    }
+
+    [Benchmark]
+    public float[,] SingleThread() => Matrix.Multiply(m1, m2);
+
+    [Benchmark]
+    public float[,] SingleThreadTransposed() => Matrix.Multiply(m2, m3, true);
+
+    [Benchmark]
+    public float[,] MultiThread() => Matrix.ThreadedMultiply(m3, m4);
+
+    [Benchmark]
+    public float[,] MultiThreadTransposed() => Matrix.ThreadedMultiply(m3, m4, true);
+
+    private void FillRandom(ref float[,] result)
+    {
+        var rand = new Random(DateTime.Now.Millisecond);
+
+        for (int i = 0; i < result.GetLength(0); i++)
+            for (int j = 0; j < result.GetLength(1); j++)
+                result[i, j] = rand.NextSingle();
+    }
+}
+
+public static class Prog
 {
     public static void Main()
     {
-        var m1 = new float[2, 5];
-        var m2 = new float[5, 5];
-
-        FillRandom(ref m1);
-        FillRandom(ref m2);
-
-        var start = DateTime.Now;
-
-        System.Console.WriteLine(m1.AsString() + '\n' + m2.AsString());
-        var s1 = Matrix.ThreadedMultiply(m1, m2, true).AsString();
-        System.Console.WriteLine(s1);
-
-        System.Console.WriteLine(DateTime.Now - start);
-
-        FillRandom(ref m1);
-        FillRandom(ref m2);
-
-        start = DateTime.Now;
-        
-        System.Console.WriteLine(m1.AsString() + '\n' + m2.AsString());
-        var s2 = Matrix.Multiply(m1, m2, true).AsString();
-        System.Console.WriteLine(s2);
-
-        System.Console.WriteLine(DateTime.Now - start);
-
-        // void Print(List<int[,]> matrix)
-        // {
-        //     foreach (var item in matrix)
-        //         for (var i = 0; i < item.GetLength(0); i++)
-        //         {
-        //             for (int j = 0; j < item.GetLength(1); j++)
-        //                 System.Console.Write("{0,-10}", item[i, j]);
-        //             System.Console.WriteLine();
-        //         }
-        //     System.Console.WriteLine();
-        // }
-
-        void FillRandom(ref float[,] result)
-        {
-            var rand = new Random(DateTime.Now.Millisecond);
-
-            for (int i = 0; i < result.GetLength(0); i++)
-                for (int j = 0; j < result.GetLength(1); j++)
-                    result[i, j] = rand.NextSingle();
-        }
+        SimpleBench sb = new();
+        System.Console.WriteLine(sb.MultiThread().AsString());
+        System.Console.WriteLine(sb.MultiThreadTransposed().AsString());
+        // var some = BenchmarkRunner.Run<SimpleBench>();
     }
 }
